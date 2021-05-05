c08ae53d218d0c8272e5c3ef7d67f0995a2e3c0e
Nickolay Tiunov <tiunov.li.174@gmail.com>
2021-05-05 14:25:00 +0300

    Implemented magic packet creator.

---
+++ magic_packet.go
@@ -0,0 +1,20 @@
+package main
+
+import "net"
+
+func Create(mac string) ([]byte, error) {
+	buf := make([]byte, 0, 6*17)
+	macBytes, err := net.ParseMAC(mac)
+	if err != nil {
+		return nil, err
+	}
+	for i := 0; i < 6; i++ {
+		buf = append(buf, 0xFF)
+	}
+	for i := 0; i < 16; i++ {
+		for _, c := range macBytes {
+			buf = append(buf, c)
+		}
+	}
+	return buf, nil
+}
